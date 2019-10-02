d03d6fc3d0fec532b709c2226d5573047e889576
Ari Ben-Elazar <abenelazar@gmail.com>
2019-10-02 17:57:49 -0400

    added create pod

--- client/kube/kube.go
+++ client/kube/kube.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"sync"
@@ -12,6 +13,7 @@ import (
 	blueclient "github.com/kolobok01/util/client"
 
 	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 
 	"k8s.io/client-go/kubernetes"
@@ -64,22 +66,54 @@ func CreateCompatibleClient(onVersionSpecified, onVersionDetermined, onUsingDefa
 }
 
 func (k *KubeClient) GetType() string {
+	if k.debug {
+		log.Printf("DEBUG: type is %s", k.Type)
+	}
 	return k.Type
 }
 
 func (k *KubeClient) GetLogs(ctx context.Context, id string) (io.ReadCloser, error) {
+	if k.debug {
+		log.Printf("DEBUG: Starting GetLogs for ID: %s", id)
+		log.Printf("DEBUG: context %+v", ctx)
+	}
 	req := k.PodManager.GetLogs(id, nil)
 	res := req.Do()
 	logs, err := res.Raw()
 	if err != nil {
+		if k.debug {
+			log.Printf("DEBUG: error in GetLogs for ID: %s", id)
+			log.Printf("DEBUG: error %s", err.Error())
+		}
 		return nil, err
 	}
 	r := ioutil.NopCloser(bytes.NewReader(logs))
-	return r, err
+	if k.debug {
+		log.Printf("DEBUG: success in GetLogs for ID: %s", id)
+		log.Printf("DEBUG: logs: %b", logs)
+	}
+	return r, nil
 }
 
 func (k *KubeClient) SetDebug(debug bool) {
 	k.mu.Lock()
 	defer k.mu.Unlock()
 	k.debug = debug
+}
+
+func (k *KubeClient) LaunchPod(name string, podSpec *apiv1.PodSpec) error {
+	if k.debug {
+		log.Printf("DEBUG: LaunchPod: PodSpec: %+v", podSpec)
+	}
+
+	_, err := k.PodManager.Create(&apiv1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: *podSpec,
+	})
+	if k.debug {
+		log.Printf("DEBUG: LaunchPod: err: %+v", err)
+	}
+	return err
 }
